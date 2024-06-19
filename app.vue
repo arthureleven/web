a134933e18aacfe421aa6f0c3d9f8cc35ae76987
arthureleven <172940445+arthureleven@users.noreply.github.com>
2024-06-19 00:03:42 -0300

    feat: add meta tags

--- app.vue
+++ app.vue
@@ -9,12 +9,20 @@ useHead({
   title: 'arthureleven/web',
   meta: [
     {
-      name: 'author',
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:title',
       content: 'arthureleven',
     },
     {
-      name: 'description',
+      name: 'twitter:description',
       content: 'My personal webpage',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://github.com/arthureleven.png',
     },
   ],
 })
